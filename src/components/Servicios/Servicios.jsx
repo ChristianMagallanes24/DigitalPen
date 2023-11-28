f54627b956fbd1023bca5c99b0d0bdccdedd276c
Christian magallanes <desarrollowebfront@gmail.com>
2023-11-28 15:44:14 -0300

    hola

--- src/components/Servicios/Servicios.jsx
+++ src/components/Servicios/Servicios.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import redes from '../../assets/redes.avif';
 import web from '../../assets/web.avif';
-import elegir from "../../assets/f.png";
+
 function Servicios() {
   return (
     <>
@@ -39,13 +39,7 @@ function Servicios() {
         <h3>Tus ideas las hacemos realidad!</h3>
 
       </div>
-      <div className="porQueElegirnos">
-      <img
-              src={elegir}
-              alt="DigitalPen Logo"
-              className="elegir"
-            />
-      </div>
+    
     </>
   );
 }
