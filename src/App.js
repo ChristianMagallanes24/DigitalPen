9df41c1ca329a05e05473d5231ae17d3401e81e9
Christian magallanes <desarrollowebfront@gmail.com>
2024-01-15 08:54:05 -0300

    se quito el chatbot

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 // App.js
-import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import Header from './components/Header/Header';
 import Servicios from './components/Servicios/Servicios';
 import Footer from './components/Footer/Footer';
-import FloatingChatButton from './components/FloatingChatButton/FloatingChatButton';
-import ChatApp from './components/ChatApp';
 import Precios from "./components/Precios/Precios";
 import { LanguageProvider } from './components/LanguageContext'; // Importar el proveedor de idioma
 
 function App() {
-  const [isChatOpen, setIsChatOpen] = useState(false);
 
-  const handleToggleChat = () => {
-    setIsChatOpen(!isChatOpen);
-  };
 
   return (
     <LanguageProvider>
@@ -24,8 +17,6 @@ function App() {
         <Servicios />
         <Precios />
         <Footer />
-        <FloatingChatButton isChatOpen={isChatOpen} onToggleChat={handleToggleChat} />
-        {isChatOpen && <ChatApp onClose={handleToggleChat} />}
       </div>
     </LanguageProvider>
   );
